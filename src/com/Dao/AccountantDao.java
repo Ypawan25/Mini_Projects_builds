5ab8199dcd5f86e5201e6716b6f58a11fc1fd821
Pawan Kumar Yadav <impawankryadav@gmail.com>
2022-12-15 19:09:58 +0530

    delete src direcory

--- src/com/Dao/AccountantDao.java
+++
@@ -1,8 +0,0 @@
-package com.Dao;
-
-import com.modal.Accountant;
-
-public interface AccountantDao {
-	
-	public Accountant accountantLogin(String username, String password);
-}
--- src/com/modal/Account.java
+++
@@ -1,5 +0,0 @@
-package com.modal;
-
-public class Account {
-
-}
